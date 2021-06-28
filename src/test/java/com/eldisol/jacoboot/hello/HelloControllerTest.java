2d06a10117d503435a63d522ffd9e40aec288040
Didier <didorg@gmail.com>
2021-06-28 17:59:51 -0400

    add: @WebMvcTest — for testing the controller
    @SpringBootTest loads complete application and injects all the beans which can be slow.

--- src/test/java/com/eldisol/jacoboot/hello/HelloControllerTest.java
+++ src/test/java/com/eldisol/jacoboot/hello/HelloControllerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,7 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
+@WebMvcTest
 @AutoConfigureMockMvc
 public class HelloControllerTest {
 
